4bac78e9f3857a0fdc210c5b87f5bbf5d9d53cac
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2026-01-31 07:34:58 +0530

    analytics

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ const isDev = process.env.NODE_ENV === 'development';
 
 export default defineConfig( {
   base: isDev ? '/' : '/xoxo/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        game: 'game.html',
+      },
+    },
+  },
   server: {
     port: 3000,
   },
